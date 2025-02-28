4f563f4a6c9c333ef47d2ed94dfe992f511ce7d8
Russ Conte <russconte@mac.com>
2025-02-27 18:58:33 -0600

    Deleted the row that created rowMeans in the ensemble. This was creating errors in the models. All models functioned correctly in testing once this one line of code was removed.

--- R/NumericEnsembles.R
+++ R/NumericEnsembles.R
@@ -2549,7 +2549,6 @@ for (i in 1:numresamples) {
     "XGBoost" = y_hat_xgb * 1 / xgb_holdout_RMSE_mean
   )
 
-  ensemble$Row_mean <- rowMeans(ensemble)
   ensemble$y_ensemble <- c(test$y, validation$y)
   y_ensemble <- c(test$y, validation$y)
 
